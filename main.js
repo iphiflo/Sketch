0620ed5b4775a0fa48394b172d6374b8f724be3a
iphiflo <87710883+iphiflo@users.noreply.github.com>
2022-02-22 15:14:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function setup()
+{
+  canvas= createCanvas(300, 300);
+  canvas.center();
+  background("white");
+  canvas.mouseReleased(classifyCanvas);
+  synth=window.speechSynthesis;
+}
+
+function clearCanvas()
+{
+    background("white")
+}
+
+function preload()
+{
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function draw()
+{
+    strokeWeight(10);
+    stroke(0);
+    if(mouseIsPressed)
+    {
+        line(pmouseX, pmouseY, mouseX, mouseY)
+    }
+}
+
+function classifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById("label").innerHTML = "label: "+results[0].label;
+}
